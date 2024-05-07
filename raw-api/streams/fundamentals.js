d5f01aeb96ad76d8b169ebd1248f32277b04d82d
mmjck <msantosjck@gmail.com>
2024-05-07 18:53:04 -0400

    raw api with nodejs

---
+++ raw-api/streams/fundamentals.js
@@ -0,0 +1,36 @@
+import { Readable, Writable, Transform, Duplex } from "node:stream"
+
+class OneToHundredStream extends Readable {
+    index = 1
+
+    _read(){
+        const i = this.index++
+
+        if (i > 100){
+            this.push(null)
+        }else {
+            const buf = new Buffer.from(String(i))
+            this.push(buf)
+        }
+    }
+}
+
+class InverseNumberStream extends Transform {
+    _transform(chunk, encoding, callback){
+        const transformed = Number(chunk.toString()) * -1
+        callback(null, Buffer.from(String(transformed)))
+    }
+}
+
+class MultiplyByTen extends Writable {
+    _write(chunk, encoding, callback){
+        console.log(Number(chunk.toString()) * 10)
+        callback()
+    }
+}
+
+
+
+new OneToHundredStream()
+.pipe(new InverseNumberStream())
+.pipe(new MultiplyByTen())
