4228c74ef453b648a9c193f7f62e44cb9ed38510
mmjck <msantosjck@gmail.com>
2024-08-10 15:13:12 -0400

    removed

--- gyms/src/use-cases/factories/make-create-gym-use-case.ts
+++
@@ -1,8 +0,0 @@
-import { CreateGymUseCase } from '../create-gym'
-import { PrismaGymsRepository } from '@/repositories/prisma/prisma-gyms-repository'
-
-export function makeCreateGymsUseCase() {
-  const repository = new PrismaGymsRepository()
-  const useCase = new CreateGymUseCase(repository)
-  return useCase
-}
--- gyms/src/use-cases/factories/make-search-gyms-use-case.ts
+++
@@ -1,8 +0,0 @@
-import { SearchGymUseCase } from '../search-gyms'
-import { PrismaGymsRepository } from '@/repositories/prisma/prisma-gyms-repository'
-
-export function makeSearchGymsUseCase() {
-  const repository = new PrismaGymsRepository()
-  const useCase = new SearchGymUseCase(repository)
-  return useCase
-}
