d64c7b8adb3fe0db03d7165bc73e53ebc1cb7695
mmjck <msantosjck@gmail.com>
2024-08-10 15:11:37 -0400

    removed

--- gyms/src/use-cases/factories/make-authenticate-use-case.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
-import { AuthenticateUseCase } from '../authenticate'
-
-export function makeAuthenticateUseCase() {
-  const repository = new PrismaUsersRepository()
-  const useCase = new AuthenticateUseCase(repository)
-  return useCase
-}
